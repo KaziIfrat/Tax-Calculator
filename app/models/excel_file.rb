27b064048816de903a451ab1cd00d1d3d5e3fff5
Kazi Ifrat <ananyakazi92@gmail.com>
2018-10-26 19:00:51 -0400

    updated

--- app/models/excel_file.rb
+++ app/models/excel_file.rb
@@ -10,30 +10,23 @@ class ExcelFile < ApplicationRecord
   end
 
 
-
-
-  def show_employee_names
+  def employee_basics(i)
     xlsx = Roo::Spreadsheet.open(create_path, extension: :xlsx)
-    no_of_employees = xlsx.column('A')[14..18].count
-    name=xlsx.column('A')[14..18]
-    fest_bonus=xlsx.column('BP')[14..18]
-    pf=xlsx.column('BS')[14..18]
-    total_income= xlsx.column('BT')[14..18]
+    name=xlsx.column('A')[14..18][i-1]
+    fest_bonus=xlsx.column('BP')[14..18][i-1]
+    pf=xlsx.column('BS')[14..18][i-1]
+    total_income= xlsx.column('BT')[14..18][i-1]
     transport= 3000
-
-    for i in 1..no_of_employees do
     emp= Employee.new
-    emp.name=name[i-1]
-    emp.pf=pf[i-1]
-    emp.fest_bonus=fest_bonus[i-1]
+    emp.name=name
+    emp.pf=pf
+    emp.fest_bonus=fest_bonus
     emp.transport=transport
-    emp.total_income=total_income[i-1]
-    c=  emp.pf[i-1] + emp.transport + emp.fest_bonus[i-1]
-    n= emp.total_income[i-1]
-    binary_search(n,c)
-    end
-
-    end
+    emp.total_income=total_income
+    c=  emp.pf + emp.transport + emp.fest_bonus
+    n= emp.total_income
+    return emp.name, emp.pf, emp.fest_bonus,emp.transport, emp.total_income, binary_search(n,c)
+  end
 
 
     def binary_search(n,c)
@@ -42,7 +35,7 @@ class ExcelFile < ApplicationRecord
     basic=0
     while low <= high
       basic= (low + high) / 2
-      g=basic+ [300000,basic/2].min + [120000,basic/10].min + c
+      g=basic + [300000,basic/2].min + [120000,basic/10].min + c
       if g == n
         break
       elsif g > n
@@ -54,21 +47,6 @@ class ExcelFile < ApplicationRecord
     basic
     end
 
-
-
-  def show_employee_festbonus
-    xlsx = Roo::Spreadsheet.open(create_path, extension: :xlsx)
-    names= xlsx.column('BP')[14..18]
-  end
-  def show_employee_pfs
-    xlsx = Roo::Spreadsheet.open(create_path, extension: :xlsx)
-    names= xlsx.column('BS')[14..18]
-  end
-
-  def show_employee_total_incomes
-    xlsx = Roo::Spreadsheet.open(create_path, extension: :xlsx)
-    names= xlsx.column('BT')[14..18]
-  end
   def create_path
     path
   end
